66e9351e3f1d70aa59e1b58ff2ed8cca6d160c5b
m8chone <pp1415@hotmail.com>
2020-03-06 21:17:06 +0700

    main activity

--- app/src/main/java/com/example/mathsquare/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mathsquare/ui/main/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mathsquare.ui.main
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mathsquare.R
 
@@ -12,12 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
 }
